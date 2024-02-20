829330e2420713d831c0e5d2a27070d94d3aba0a
Salvador Gonzalez <salvadorgonmo@gmail.com>
2024-02-20 13:24:31 -0600

    fix: fixes issue with graphs

--- src/AudioVisualizer.jsx
+++ src/AudioVisualizer.jsx
@@ -12,6 +12,9 @@ class AudioVisualiser extends Component {
       return;
     }
 
+    if (this.props.strokeStyle === 'bar') {
+      this.drawBar()
+    }
 
   }
 
@@ -24,7 +27,7 @@ class AudioVisualiser extends Component {
     let x = 0;
     const sliceWidth = (width * 1.0) / audioData.length;
 
-    context.lineWidth = 5;
+    context.lineWidth = 10;
 // Create linear gradient
 const gradient = context.createLinearGradient(0, 0, width, 0);
 gradient.addColorStop(0, '#0E54CF');
@@ -47,31 +50,35 @@ context.strokeStyle = gradient;
   }
 
   drawBar = () => {
-    const { audioData, analyser } = this.props;
-    const bufferLength = analyser.frequencyBinCount;
+    const { audioData } = this.props;
     const canvas = this.canvas.current;
     const height = canvas.height;
     const width = canvas.width;
     const context = canvas.getContext('2d');
-    let x = 0;
-    const sliceWidth = (width * 1.0) / audioData.length;
-    canvas.clearRect(0, 0, width, height);
-    canvas.fillStyle = "rgb(0 0 0)";
-    canvas.fillRect(0, 0, width, height);
-    const barWidth = (width / bufferLength) * 2.5;
-let barHeight;
-for (let i = 0; i < bufferLength; i++) {
-  barHeight = audioData[i] / 2;
+    const numBars = audioData.length;
+    const barWidth = width / numBars;
+    
+    // Create linear gradient
+    const gradient = context.createLinearGradient(0, height, 0, 0);
+    gradient.addColorStop(0, '#0E54CF');
+    gradient.addColorStop(1, '#3EC1A5');
+    
+    // Clear canvas
+    context.clearRect(0, 0, width, height);
+    
+    // Draw bars
+    for (let i = 0; i < numBars; i++) {
+      const barHeight = (audioData[i] / 255.0) * height;
+      const x = i * barWidth;
+      const y = height - barHeight; // Start from the bottom
+      context.fillStyle = gradient;
+      context.fillRect(x, y, barWidth, barHeight);
+    }
 
-  canvas.fillStyle = `rgb(${barHeight + 100} 50 50)`;
-  canvas.fillRect(x, height - barHeight / 2, barWidth, barHeight);
-
-  x += barWidth + 1;
-}
   }
 
   render() {
-    return <canvas width="300" height="300" ref={this.canvas} />;
+    return <canvas style={{ filter: 'blur(5px)'}} width="300" height="300" ref={this.canvas} />;
   }
 }
 
